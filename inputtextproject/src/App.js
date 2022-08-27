cddf6a1605f415a228d3e35db09fc44e2d9e085e
Pezhman Taghipour <p.taghipour@irisad.net>
2022-08-27 12:35:48 +0430

    this is first commit

---
+++ inputtextproject/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<div>
+    <div><h1>This is my first react project</h1></div>
+    <div><h1>This is my first react project</h1></div>
+    </div>
+
+
+    // <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+    // </div>
+  );
+}
+
+export default App;

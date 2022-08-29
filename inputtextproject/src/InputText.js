f5835775f0f7c5e4afb589bf521aa411d6d6a99d
Pezhman Taghipour <p.taghipour@irisad.net>
2022-08-29 13:07:37 +0430

    add change value method

--- inputtextproject/src/InputText.js
+++ inputtextproject/src/InputText.js
@@ -4,19 +4,22 @@ import { useState } from 'react';
 const App = () => {
 
     const [status, setStatus] = useState(false);
-    const [textvalue, setTextvalue] = useState('hi');
+    const [textvalue, setTextvalue] = useState('');
     const changeStat = () => {
         setStatus(!status);
     };
+
+const changValue=(event)=>{
+setTextvalue(event.target.value)
+}
 
     return (
         <div>
             {!status ? <input
                 type="text" id="message" value={textvalue}
-                onChange={(event) => setTextvalue(event.target.value)} /> : <span > {textvalue}</span>}
+                onChange={changValue} /> : <span > {textvalue}</span>}
             <button onClick={changeStat}>{status ? 'EnableEdit' : 'DisableEdit'}</button>
         </div>
     );
-};
-
+}; 
 export default App;

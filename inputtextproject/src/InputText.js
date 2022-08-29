d1d4bb2c6fb2e9f3c7209483484dd72640de39be
Pezhman Taghipour <p.taghipour@irisad.net>
2022-08-29 10:43:42 +0430

    chnage inputText.js

--- inputtextproject/src/InputText.js
+++ inputtextproject/src/InputText.js
@@ -4,22 +4,17 @@ import { useState } from 'react';
 const App = () => {
 
     const [status, setStatus] = useState(false);
-
+    const [textvalue, setTextvalue] = useState('hi');
     const changeStat = () => {
-        setStatus(!status.isReadOnly);
-        console.log('HI PEJMAN')
+        setStatus(!status);
     };
 
     return (
         <div>
-            <input
-                type="text"
-                id="message"
-                name="message"
-                disabled={status}
-            />
-
-            <button onClick={changeStat}>ChangeStatus</button>
+            {!status ? <input
+                type="text" id="message" value={textvalue}
+                onChange={(event) => setTextvalue(event.target.value)} /> : <span > {textvalue}</span>}
+            <button onClick={changeStat}>{status ? 'EnableEdit' : 'DisableEdit'}</button>
         </div>
     );
 };
